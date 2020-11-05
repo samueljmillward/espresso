ef79982f4ad95487e51cb7a13a2e991b07e902d9
samueljmillward <samuel.j.pearce@hotmail.com>
2020-11-05 16:39:40 +0000

    Styled Navigation

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -4,11 +4,84 @@ import styled from "styled-components";
 const Navbar = () => {
   return (
     <Nav>
-      <h1>Navbar</h1>
+      <Logo href="">
+        Es<span>presso</span>
+      </Logo>
+
+      <Icon>
+        <span />
+        <span />
+        <span />
+      </Icon>
+
+      <Menu>
+        <MenuLink href="">Your Beans</MenuLink>
+        <MenuLink href="">Pull A Shot</MenuLink>
+      </Menu>
     </Nav>
   );
 };
 
 export default Navbar;
 
-const Nav = styled.div``;
+const MenuLink = styled.a`
+  padding: 1rem 2rem;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  color: burlywood;
+  transition: all 0.3s ease-in;
+  font-size: 0.9rem;
+
+  &:hover {
+    color: #c46404;
+  }
+`;
+
+const Nav = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  background: #642b09;
+`;
+
+const Logo = styled.a`
+  padding: 1rem 0;
+  color: #af7c35;
+  text-decoration: none;
+  font-weight: 800;
+  font-size: 1.7rem;
+
+  span {
+    font-weight: 800;
+    font-size: 1.7rem;
+    color: #af7c35;
+  }
+`;
+
+const Icon = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+
+  span {
+    height: 2px;
+    width: 25px;
+    background: burlywood;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`;
