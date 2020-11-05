af425f3c6af070b225f2d73000dc1843aee881a2
samueljmillward <samuel.j.pearce@hotmail.com>
2020-11-05 16:37:52 +0000

    Scaffolded components

---
+++ src/elements/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <h1>Navbar</h1>
+    </Nav>
+  );
+};
+
+export default Navbar;
+
+const Nav = styled.div``;
---
+++ src/styles/StyledApp.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const AppStyled = styled.div`
+  background: #c46404;
+  height: 100vh;
+`;
