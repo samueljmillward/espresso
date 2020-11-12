a7775a4fbdcf2c548655856d5a64ab36ab876e08
samueljmillward <samuel.j.pearce@hotmail.com>
2020-11-12 21:34:16 +0000

    Routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
 import GlobalStyle from './styles/GlobalStyle';
-// import Login from './pages/login';
 import Home from './pages/home';
-import { BrowserRouter, Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { StyledApp } from './styles/StyledApp';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <GlobalStyle />
       <StyledApp>
         {/* <Login /> */}
-        <Home />
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
       </StyledApp>
-    </>
+    </Router>
   );
 };
 
