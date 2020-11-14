744f7c5525ecf6b7a3802896a45a26cdf8908e70
samueljmillward <samuel.j.pearce@hotmail.com>
2020-11-14 14:09:20 +0000

    Login validation

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,86 +4,104 @@ import { Button, TextField, Grid, InputAdornment } from '@material-ui/core';
 import { AccountCircle, LockRounded } from '@material-ui/icons';
 import { StyledApp } from '../styles/StyledApp';
 import GlobalStyle from '../styles/GlobalStyle';
+import { useForm } from 'react-hook-form';
 
 import espresso from '../images/espresso.jpg';
 
 const Login = () => {
+  const { register, errors, handleSubmit } = useForm({ mode: 'onChange' });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <GlobalStyle />
       <StyledApp>
-        <Grid container style={{ minheight: '100vh' }}>
-          <Grid
-            container
-            item
-            xs={12}
-            sm={6}
-            alignItems="center"
-            direction="column"
-            justify="space-between"
-            style={{ padding: 40 }}
-          >
-            <div />
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                maxWidth: 400,
-                minWidth: 300,
-              }}
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+          <Grid container style={{ minheight: '100vh' }}>
+            <Grid
+              container
+              item
+              xs={12}
+              sm={6}
+              alignItems="center"
+              direction="column"
+              justify="space-between"
+              style={{ padding: 40 }}
             >
-              <Grid container justify="center">
-                <h2 style={{ color: 'burlywood' }}>Espresso Tracker</h2>
+              <div />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: 400,
+                  minWidth: 300,
+                }}
+              >
+                <Grid container justify="center">
+                  <h2 style={{ color: 'burlywood' }}>Espresso Tracker</h2>
+                </Grid>
+                <TextField
+                  inputRef={register({ required: true, maxLength: 20 })}
+                  name="Username"
+                  label="Username"
+                  margin="normal"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <AccountCircle />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <TextField
+                  inputRef={register({ required: true })}
+                  name="Password"
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockRounded />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <div style={{ height: 20 }} />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  style={{ marginBottom: '.8rem' }}
+                >
+                  Log In
+                </Button>
+                <Button
+                  variant="outlined"
+                  component={RouterLink}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </div>
+              <Grid item>
+                <Button variant="outlined">Forgot Password?</Button>
               </Grid>
-              <TextField
-                label="Username"
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <AccountCircle />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <TextField
-                label="Password"
-                type="password"
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockRounded />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <div style={{ height: 20 }} />
-              <Button variant="contained" style={{ marginBottom: '.8rem' }}>
-                Log In
-              </Button>
-              <Button variant="outlined" component={RouterLink} to="/register">
-                Register
-              </Button>
-            </div>
-            <Grid item>
-              <Button variant="outlined">Forgot Password?</Button>
+              <div />
             </Grid>
-            <div />
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              display={{ xs: 'none', lg: 'block', xl: 'none' }}
+            >
+              <img
+                src={espresso}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                alt="background"
+              />
+            </Grid>
           </Grid>
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            display={{ xs: 'none', lg: 'block', xl: 'none' }}
-          >
-            <img
-              src={espresso}
-              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-              alt="background"
-            />
-          </Grid>
-        </Grid>
+        </form>
       </StyledApp>
     </div>
   );
