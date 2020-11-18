3d7e08eaccb1e34b73451a18cadc14e549a74d2f
samueljmillward <samuel.j.pearce@hotmail.com>
2020-11-18 15:15:58 +0000

    Card component

--- src/pages/beans.js
+++ src/pages/beans.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import Navbar from '../elements/Navbar';
 
+import BeanCard from '../elements/Card.js';
+
 const Beans = () => {
   return (
     <>
       <Navbar />
+      <BeanCard />
     </>
   );
 };
