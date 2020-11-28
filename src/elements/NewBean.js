5859fad6130cb96991ae7cf0e165ba7b3b7354cb
samueljmillward <samuel.j.pearce@hotmail.com>
2020-11-28 14:18:37 +0000

    React Hook Form

--- src/elements/NewBean.js
+++ src/elements/NewBean.js
@@ -1,32 +1,25 @@
 import React from 'react';
-import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Grid from '@material-ui/core/Grid';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import DateFnsUtils from '@date-io/date-fns';
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import { useForm, Controller } from 'react-hook-form';
 
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-  textField: {
-    width: '25ch',
-  },
   addButton: {
     display: 'flex',
     justifyContent: 'flex-end',
@@ -36,13 +29,22 @@ const useStyles = makeStyles((theme) => ({
 
 export default function NewBean() {
   const classes = useStyles();
+
+  const { register, handleSubmit, control } = useForm();
+
+  const onSubmit = (data) => {
+    handleClose();
+    console.log(data);
+  };
+
+  const [open, setOpen] = React.useState(false);
+
   const [selectedDate, setSelectedDate] = React.useState(
     new Date('2020-11-22T21:11:54')
   );
-  const [open, setOpen, values, setValues] = React.useState({
-    amount: '',
+
+  const [values, setValues] = React.useState({
     weight: '',
-    weightRange: '',
   });
 
   const handleDateChange = (date) => {
@@ -77,52 +79,51 @@ export default function NewBean() {
             To subscribe to this website, please enter your email address here.
             We will send updates occasionally.
           </DialogContentText> */}
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Name"
-            type="text"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="origin"
-            label="Origin"
-            type="text"
-            fullWidth
-          />
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="space-around">
-              <KeyboardDatePicker
-                disableToolbar
-                variant="inline"
-                format="MM/dd/yyyy"
-                margin="normal"
-                id="date-picker-inline"
-                label="Date picker inline"
-                value={selectedDate}
-                onChange={handleDateChange}
-                fullWidth
-                KeyboardButtonProps={{
-                  'aria-label': 'change date',
-                }}
-              />
-              <TextField
-                autoFocus
-                margin="dense"
-                id="flavours"
-                label="Flavours"
-                type="text"
-                fullWidth
-              />
-            </Grid>
-          </MuiPickersUtilsProvider>
-          <FormControl
-            className={clsx(classes.margin, classes.textField)}
-            variant="outlined"
-          >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              autoFocus
+              margin="dense"
+              inputRef={register}
+              id="name"
+              label="Name"
+              type="text"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              inputRef={register}
+              id="origin"
+              label="Origin"
+              type="text"
+              fullWidth
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Grid container justify="space-around">
+                <KeyboardDatePicker
+                  disableToolbar
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="date-picker-inline"
+                  label="Date picker inline"
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  fullWidth
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
+                />
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="flavours"
+                  label="Flavours"
+                  type="text"
+                  fullWidth
+                />
+              </Grid>
+            </MuiPickersUtilsProvider>
             <OutlinedInput
               id="outlined-adornment-weight"
               onChange={handleChange('weight')}
@@ -136,13 +137,13 @@ export default function NewBean() {
             <FormHelperText id="outlined-weight-helper-text">
               Weight
             </FormHelperText>
-          </FormControl>
+          </form>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button type="submit" onClick={onSubmit} color="primary">
             Submit
           </Button>
         </DialogActions>
