dc4d9812fb98c339f288f00a9f438bb772ca43c4
samueljmillward <samuel.j.pearce@hotmail.com>
2021-07-06 12:03:42 +0100

    Add document title with React-Helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Helmet } from 'react-helmet';
+
 import Home from './pages/home';
 import Beans from './pages/beans';
 import Brew from './pages/brews';
@@ -11,16 +13,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
-      <GlobalStyle />
-      <StyledApp>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/beans' exact component={Beans} />
-          <Route path='/brews' component={Brew} />
-        </Switch>
-      </StyledApp>
-    </Router>
+    <>
+      <Helmet>
+        <title>Espresso Tracker</title>
+      </Helmet>
+      <Router>
+        <GlobalStyle />
+        <StyledApp>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/beans' exact component={Beans} />
+            <Route path='/brews' component={Brew} />
+          </Switch>
+        </StyledApp>
+      </Router>
+    </>
   );
 };
 
